764a3272c3811b4da77e16a37e31cadfcf71ea83
julilka <bugsbunnnyy@gmail.com>
2023-11-13 19:29:06 +0200

    code-refactoring

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,12 +1,17 @@
-import './Card.css'
+import React, {ReactNode} from 'react';
+import './Card.css';
 
-const Card = (props: any) => {
+interface ICardProps {
+    className?: string;
+    children: ReactNode;
+}
+
+const Card: React.FC<ICardProps> = (props) => {
     const addedClass = props.className || '';
     const classes = 'card ' + addedClass;
     return (
         <div className={classes}>{props.children}</div>
-    )
+    );
 }
-
 
 export default Card;
