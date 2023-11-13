75ed2264825b8ab10a89706e9a5486b881371e48
julilka <bugsbunnnyy@gmail.com>
2023-11-13 19:32:38 +0200

    code-refactoring

--- src/Router/PrivateRoutes.tsx
+++ src/Router/PrivateRoutes.tsx
@@ -1,11 +1,12 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {useAuth} from "../hook/useContext";
+import {FC} from "react";
 
-const PrivateRoutes = () => {
-    const {isLoggedIn} = useAuth()
+const PrivateRoutes: FC = () => {
+    const {isLoggedIn} = useAuth();
     return (
         isLoggedIn ? <Outlet/> : <Navigate to={"login"}/>
-    )
-}
+    );
+};
 
 export default PrivateRoutes;
--- src/shared/FetchApi.tsx
+++ src/shared/FetchApi.tsx
@@ -1,6 +1,15 @@
 import {DEFAULT_FEEDS_URL, PROXY_SERVER_URL} from "./DefaultUrls";
 
-async function fetchUsers() {
+interface Post {
+    id: number;
+    data: {
+        title: string;
+        body: string;
+        userId: number;
+    };
+}
+
+async function fetchUsers(): Promise<any> {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users', {
             method: 'GET',
@@ -17,29 +26,31 @@ async function fetchUsers() {
     }
 }
 
-async function fetchUserPost() {
+async function fetchUserPost(): Promise<Post[] | undefined> {
     const userId = localStorage.getItem('userID');
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+    if (userId) {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-    if (response.ok) {
-        const userData = await response.json();
-        return userData.map(item => ({
-            id: item.id,
-            data: {
-                title: item.title,
-                body: item.body,
-                userId: item.userId
-            }
-        }));
+        if (response.ok) {
+            const userData = await response.json();
+            return userData.map((item: any) => ({
+                id: item.id,
+                data: {
+                    title: item.title,
+                    body: item.body,
+                    userId: item.userId,
+                },
+            }));
+        }
     }
 }
 
-async function fetchFeeds(url, index) {
+async function fetchFeeds(url: string, index: number): Promise<{ id: number; data: any } | undefined> {
     try {
         const response = await fetch(PROXY_SERVER_URL, {
             method: 'POST',
@@ -50,13 +61,12 @@ async function fetchFeeds(url, index) {
         });
         const data = await response.json();
         return {id: index, data: data};
-
     } catch (error) {
         console.error('Error fetching the feed:', error);
     }
 }
 
-async function addNewFeed(newFeedUrl) {
+async function addNewFeed(newFeedUrl: string): Promise<any> {
     try {
         const response = await fetch(PROXY_SERVER_URL, {
             method: 'POST',
@@ -74,18 +84,20 @@ async function addNewFeed(newFeedUrl) {
     }
 }
 
-async function setDataStorage() {
+async function setDataStorage(): Promise<void> {
     // if (!localStorage.getItem('feeds') || !localStorage.getItem('posts')) {
     Promise.all(DEFAULT_FEEDS_URL.map((url, index) => fetchFeeds(url, index)))
         .then((res) => {
             localStorage.setItem('feeds', JSON.stringify(res));
         })
-        .catch(error => console.error('Error fetching feeds:', error));
+        .catch((error) => console.error('Error fetching feeds:', error));
 
-    fetchUserPost().then(formattedData => {
-        localStorage.setItem('posts', JSON.stringify(formattedData));
+    fetchUserPost().then((formattedData) => {
+        if (formattedData) {
+            localStorage.setItem('posts', JSON.stringify(formattedData));
+        }
     });
     // }
 }
 
-export {fetchUsers, fetchFeeds, fetchUserPost, addNewFeed, setDataStorage}
+export {fetchUsers, fetchFeeds, fetchUserPost, addNewFeed, setDataStorage};
