935fbd69a558eb57534a3fa8bba318e8f2abc924
julilka <bugsbunnnyy@gmail.com>
2023-11-14 16:36:52 +0200

    code-refactoring.

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import styles from "./LoginForm.module.css";
 import {useNavigate} from "react-router-dom";
 import {useAuth} from "../../shared/useContext";
